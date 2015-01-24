d665002f50df70befe7ea4f8dae27e42832cb5e6
Adrien AUBRY <aaubry@e3r6p12.42.fr>
2015-01-24 18:52:37 +0100

    err_to_stash

--- gelua/ui.Button.lua
+++ gelua/ui.Button.lua
@@ -1,22 +1,2 @@
-Button = class("Button")
-
-function Button:init(str, cb)
-	self.text = str
-	self.cb = cb
-end
-
-function Button:fullText()
-	return self.text
-end
-
-function Button:render(font)
-	if font ~= nil then
-		w, h = font:measureString(self.text)
-		screen.defaultShader:use()
-		font:print(self.textx * screen.width, self.texty * screen.height, self.text)
-	else
-		w, h = screen.font[FONT_SIZE_NORMAL]:measureString(self.text)
-		screen.defaultShader:use()
-		screen.font[FONT_SIZE_NORMAL]:print(self.textx * screen.width, self.texty * screen.height, self.text)
-	end
-end
+const char h_uiButton[] = 
+;
